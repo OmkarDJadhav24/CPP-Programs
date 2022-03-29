c4d0963703dceb30e472a9c3e8f0bb8095d652eb
OmkarDJadhav24 <101233431+OmkarDJadhav24@users.noreply.github.com>
2022-03-29 21:52:12 +0530

    Add files via upload

---
+++ dynamic.c
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int Arr[10];
+	int *p=NULL;
+	
+	p=(int *)malloc(sizeof(int)*10);   //for c
+	
+	p=new int[10];
+	
+	if(p==NULL)
+	{
+		cout<<"\nUnable to allocate the memory";
+	}
+	else
+	{
+		cout<<"\nMemory gets allocated successfully at address:"<<p<<"\n";
+	}
+	
+	return 0;
+	
+}
