303e9719ac8ae9d37ed2a4c5b8ec11bfc796667a
OmkarDJadhav24 <101233431+OmkarDJadhav24@users.noreply.github.com>
2022-07-10 12:51:08 +0530

    Add files via upload

--- constructor1.cpp
+++ constructor1.cpp
@@ -1,35 +1,47 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
+
 class Demo
 {
 	public:
-	int x;
-	int y;
+	int x;                                     //Characteristics
+	int y;                                     //Characteristics
 	
-	Demo()
+	Demo()                                     //Default Constructor
 	{
-		cout<<"\nInside default constructor";
+		cout<<"\nInside Default Constructor";
 		x=0;
 		y=0;
 	}
-	
-	Demo(int i,int j)
+	Demo(int i,int j)                           //Parameterized Constructor
 	{
-		cout<<"\nInside parameterised constructor";
+		cout<<"\nInside Parameterized Constructor";
 		x=i;
 		y=j;
 	}
-	Demo(Demo &ref)
+	Demo(Demo &ref)                        //Copy Constructor
 	{
-		cout<<"\nInside Copy Constructor";
-		x=ref.x;
-		y=ref.y;
+		cout<<"\nInside Destructor";
 	}
+	
+	~Demo()
+	{
+		cout<<"\nInside Destructor";
+	}
+	
 };
 int main()
 {
-   Demo obj1(11,12);
-   Demo obj2(obj1);
-   
-   return 0;
+	Demo obj1(11,12);
+	Demo obj2(51,101);
+	printf("\nSize of obj1:%d",sizeof(obj1));
+	cout<<"\nValue of x in obj1:"<<obj1.x;
+	cout<<"\nValue of y in obj1:"<<obj1.y;
+	
+	obj1.x++;
+	cout<<"\nNew value of x of obj1is:"<<obj1.x;
+	cout<<"\nNew value of x of obj2 is:"<<obj2.x;
+	
+	return 0;
 }
